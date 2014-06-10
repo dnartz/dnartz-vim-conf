452fc256ea501bf2f1b9ff7405ffe6f7faab087e
dnartz <xylon.tao@gmai.com>
2014-06-11 00:30:15 +0800

    修正了bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
 # /usr/bin/sh
 
+BASEDIR=$(cd `dirname $0`; pwd)
+
 mv -vf ~/.vimrc.before.local ~/.vimrc.before.local.old
 mv -vf ~/.vimrc.bundles.local ~/.vimrc.bundles.local.old
 mv -vf ~/.vimrc.local ~/.vimrc.local.old
 mv -vf ~/.gvimrc.local ~/.gvimrc.local.old
 
-ln -s ./vimrc.before.local ~/.vimrc.before.local
-ln -s ./vimrc.before.local ~/.vimrc.before.local
-ln -s ./vimrc.before.local ~/.vimrc.before.local
-ln -s ./vimrc.before.local ~/.vimrc.before.local
+ln -sv ${BASEDIR}/.vimrc.before.local ~/.vimrc.before.local
+ln -sv ${BASEDIR}/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sv ${BASEDIR}/.vimrc.local ~/.vimrc.local
+ln -sv ${BASEDIR}/.gvimrc.local ~/.gvimrc.local
