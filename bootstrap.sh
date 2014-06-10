5c5f53094b0993cb685f83a47a5387e5aa737ff7
dnartz <xylon.tao@gmai.com>
2014-06-11 00:09:58 +0800

    Init commit.

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+# /usr/bin/sh
+
+mv -vf ~/.vimrc.before.local ~/.vimrc.before.local.old
+mv -vf ~/.vimrc.bundles.local ~/.vimrc.bundles.local.old
+mv -vf ~/.vimrc.local ~/.vimrc.local.old
+mv -vf ~/.gvimrc.local ~/.gvimrc.local.old
+
+ln -s ./vimrc.before.local ~/.vimrc.before.local
+ln -s ./vimrc.before.local ~/.vimrc.before.local
+ln -s ./vimrc.before.local ~/.vimrc.before.local
+ln -s ./vimrc.before.local ~/.vimrc.before.local
